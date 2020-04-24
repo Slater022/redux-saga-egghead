49f31c893b6a13de57043cc68f7f108b374d1970
Thomas <Slater022@users.noreply.github.com>
2020-04-24 11:52:54 -0400

    Fix some dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { Provider } from 'react-redux';
 import configureStore from './app/store/app.store';
-import {history} from './app/store/_root.reducer';
 import RouterComponent from './app/features/router/router.component';
-import { ConnectedRouter } from 'connected-react-router';
 
 const store = configureStore();
 
